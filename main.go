84471cf742b5c1f0306ae26495ca820629cd7996
Dicky-kurnia <dickykurnia1907@gmail.com>
2022-03-19 20:22:54 +0700

    Upload avtar endpoint

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
+	userService.SaveAvatar(1, "images/foto-profile.png")
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
@@ -30,6 +32,7 @@ func main() {
 	api.POST("/users", userHandler.RegisterUSer)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run()
 }
