8c67c5a52868e986d4b4e29d64609a80ac398ecb
Dicky-kurnia <dickykurnia1907@gmail.com>
2022-03-20 01:57:37 +0700

    jwt auth, validate token

--- main.go
+++ main.go
@@ -24,8 +24,6 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
-	userService.SaveAvatar(1, "images/foto-profile.png")
-
 	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
